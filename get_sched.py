8f21dd0dd4e4b3be1a2759445835691b3135e130
Eric Miranda <ericmiranda7@gmail.com>
2021-05-05 12:21:35 +0530

    refactor the fetching

--- get_sched.py
+++ get_sched.py
@@ -13,7 +13,6 @@ client = pymongo.MongoClient("mongodb://localhost:27017")
 URL = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict'
 db = client['cowin']
 db = db["hospitals"]
-print(db)
 
 def get(dId, date):
   params = {'district_id': dId, 'date': date}
@@ -29,23 +28,51 @@ def update_db(dId, date, age):
   # iterate through centers
   for center in data:
     for sessions in center.get('sessions'):
-      list_of_sessions = []
-      list_of_sessions.append(sessions)
-      for session in list_of_sessions:
-        center_name = center.get('name')
-        center_id = center.get('center_id')
-        slots = session.get('available_capacity')
-        print(slots) 
-      
-        exists = db.find_one({'_id': center_id})
-        if not exists:
-          hospital = {'_id': center_id, 'name': center_name, 'fourtyFive': [], 'eighteen': []}
-          db.insert_one(hospital)
-        if age == 45:
-          db.update_one({'_id': center_id}, {'$push': {'fourtyFive': {session.get('date'): slots}}})
-        """ elif age == 18:
-          db.update_one({'_id': center_id}, {'$push': {'eighteen': {session.get('date'): slots}}}) """
+      curr_center = {
+        '_id': center.get('center_id'),
+        'name': center.get('name'),
+        'sessions': [],
+      }
 
+      # check if center exists in db
+      exists = db.find_one({'_id': curr_center['_id']})
+      if not exists:
+        hospital = {'_id': curr_center['_id'], 'name': curr_center['name'], 'fourtyFive': [], 'eighteen': []}
+        db.insert_one(hospital)
+      db_center = db.find_one({'_id': curr_center['_id']})
+
+
+      curr_center['sessions'].append(sessions)
+      for session in curr_center['sessions']:
+        if session.get('min_age_limit') != 45:
+          continue
+        session_slots = session.get('available_capacity')
+        session_date = session.get('date')
+
+        # check if center exists in db
+        exists = db.find_one({'_id': curr_center['_id']})
+        if not exists:
+          hospital = {'_id': curr_center['_id'], 'name': curr_center['name'], 'fourtyFive': [], 'eighteen': []}
+          db.insert_one(hospital)
+
+        # cross-check session date with local db
+        api_record = {session_date: session_slots}
+        db_record = db_center['fourtyFive']
+
+        exists = False
+        for ind, rec in enumerate(db_record):
+          for key_date, slots in rec.items():
+            if key_date == session_date:
+              if slots != session_slots:
+                # update local db
+                print('updating')
+                db.update_one({'__id': curr_center['_id']}, {'$set': {'fourtyFive.'+str(ind)+'.'+str(date): session_slots}})
+                exists = True
+        
+        if not exists:
+          # if date doesn't exist, insert it
+          db.update_one({'_id': curr_center['_id']}, {'$push': {'fourtyFive': {session_date: session_slots}}})
+        
 
 def check_for_updates(dId, age):
   date = datetime.datetime.now()

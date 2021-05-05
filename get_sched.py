17a46b719d625783cc8628b0799ade03619cb21e
Eric Miranda <ericmiranda7@gmail.com>
2021-05-05 10:32:47 +0530

    switch to local db for dev

--- get_sched.py
+++ get_sched.py
@@ -7,11 +7,13 @@ from bson.objectid import ObjectId
 from dotenv import load_dotenv
 load_dotenv()
 
-client = pymongo.MongoClient(f"mongodb+srv://fullStackOpen:{os.environ['DB_PASS']}@cluster0.utvrz.mongodb.net/cowin?retryWrites=true&w=majority")
+#client = pymongo.MongoClient(f"mongodb+srv://fullStackOpen:{os.environ['DB_PASS']}@cluster0.utvrz.mongodb.net/cowin?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb://localhost:27017")
 
 URL = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict'
 db = client['cowin']
 db = db["hospitals"]
+print(db)
 
 def get(dId, date):
   params = {'district_id': dId, 'date': date}
@@ -23,7 +25,6 @@ def get(dId, date):
 def update_db(dId, date, age):
   data = get(dId, date)
   data = data.get('centers')
-  print(data)
   
   # iterate through centers
   for center in data:
@@ -31,20 +32,19 @@ def update_db(dId, date, age):
       list_of_sessions = []
       list_of_sessions.append(sessions)
       for session in list_of_sessions:
-        if session.get('min_age_limit') == age:
-          center_name = center.get('name')
-          center_id = center.get('center_id')
-          slots = session.get('available_capacity')
-          
-          
-          exists = db.find_one({'_id': center_id})
-          if not exists:
-            hospital = {'_id': center_id, 'name': center_name, 'fourtyFive': [], 'eighteen': []}
-            db.insert_one(hospital)
-          if age == 45:
-            db.update_one({'_id': center_id}, {'$push': {'fourtyFive': {session.get('date'): slots}}})
-          """ elif age == 18:
-            db.update_one({'_id': center_id}, {'$push': {'eighteen': {session.get('date'): slots}}}) """
+        center_name = center.get('name')
+        center_id = center.get('center_id')
+        slots = session.get('available_capacity')
+        print(slots) 
+      
+        exists = db.find_one({'_id': center_id})
+        if not exists:
+          hospital = {'_id': center_id, 'name': center_name, 'fourtyFive': [], 'eighteen': []}
+          db.insert_one(hospital)
+        if age == 45:
+          db.update_one({'_id': center_id}, {'$push': {'fourtyFive': {session.get('date'): slots}}})
+        """ elif age == 18:
+          db.update_one({'_id': center_id}, {'$push': {'eighteen': {session.get('date'): slots}}}) """
 
 
 def check_for_updates(dId, age):

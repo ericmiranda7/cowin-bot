fb219d11ffa6c52ae3d2e5392895237d48acdd4c
Eric Miranda <ericmiranda7@gmail.com>
2021-05-11 16:26:53 +0530

    remove debug stuff

--- get_sched.py
+++ get_sched.py
@@ -7,8 +7,8 @@ from bson.objectid import ObjectId
 from dotenv import load_dotenv
 load_dotenv()
 
-client = pymongo.MongoClient(f"mongodb+srv://fullStackOpen:{os.environ['DB_PASS']}@cluster0.utvrz.mongodb.net/cowin?retryWrites=true&w=majority")
-#client = pymongo.MongoClient("mongodb://localhost:27017")
+#client = pymongo.MongoClient(f"mongodb+srv://fullStackOpen:{os.environ['DB_PASS']}@cluster0.utvrz.mongodb.net/cowin?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb://localhost:27017")
 
 URL = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict'
 HEADER = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
@@ -22,7 +22,7 @@ def get(dId, date) -> dict:
   return r.json()
 
 def update_db(dId, date) -> int:
-  districts = []
+  centers = {'fourtyFive': [], 'eighteen': []}
   data = get(dId, date).get('centers')
   
   # iterate through centers
@@ -61,9 +61,8 @@ def update_db(dId, date) -> int:
       if db_record is not None:
         db_slots = db_record[db_key][str(session_date)]
         if db_slots != session_slots:
-          print('updating', db_slots, session_slots)
           if db_slots < session_slots:
-            print('ie, add update')
+            centers[db_key].append({'name': curr_center.get('name'), 'slots': session_slots})
           db.update_one(
             {'_id': curr_center['_id']},
             {'$set': {db_key+'.'+str(session_date): session_slots}}
@@ -74,16 +73,16 @@ def update_db(dId, date) -> int:
             {'$set': {db_key+'.'+str(session_date): session_slots}}
         )
 
-  return districts
+  return centers
 
 
 def check_for_updates(dId) -> int:
-  districts = []
+  centers = []
   date = datetime.datetime.now()
   for i in range(4):
     updates = update_db(dId, date.strftime('%d-%m-%Y'))
     if updates:
-      districts.append(updates)
+      centers.append(updates)
     date += datetime.timedelta(weeks=1)
 
-  return districts
+  return centers
